f06600bfb1dd94c99218282eb3f843f31e9b941b
Reverier-Xu <reverier.xu@woooo.tech>
2024-06-20 00:26:36 +0800

    :sparkles: support GFM

--- web/src/routes/submit/index.tsx
+++ web/src/routes/submit/index.tsx
@@ -5,6 +5,7 @@ import { t } from "@/lib/storage/theme";
 import { addToast } from "@/lib/storage/toast";
 import { getCurrentWeek } from "@/lib/utils/time";
 import Button from "@/lib/widgets/button";
+import Card from "@/lib/widgets/card";
 import Editor from "@/lib/widgets/editor";
 import type { HTTPError } from "ky";
 import { createSignal } from "solid-js";
@@ -53,17 +54,27 @@ export default function () {
     return (
         <>
             <Title title={`${t("submit.title")} - ${t("platform.name")}`} />
-            <div class="p-3 lg:p-6 flex flex-col self-center w-full max-w-5xl flex-1">
+            <div class="p-3 lg:p-6 flex flex-col self-center w-full max-w-5xl flex-1 space-y-2">
                 <h1 class="h-12 flex flex-row space-x-2 items-center">
                     <span class="font-bold px-2 flex-1 text-start">{t("submit.title")}</span>
                     <Button size="sm" level="primary" onClick={handleSubmit} loading={loading()}>
                         {t("form.submit")}
                     </Button>
                 </h1>
+                <Card level="info" contentClass="p-2">
+                    <p>{t("submit.placeholder")}</p>
+                    <a
+                        class="inline-flex space-x-2 items-center"
+                        href="https://docs.github.com/zh/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax"
+                    >
+                        <span>GitHub Favored Markdown Tutorial</span>
+                        <span class="icon-[fluent--open-20-regular] w-5 h-5 text-primary" />
+                    </a>
+                </Card>
                 <Editor
                     class="flex-1"
                     lineNumbers
-                    placeholder={t("submit.placeholder") as string}
+                    placeholder="MARKDOWN"
                     error={hasError() ? (t("submit.required") as string) : undefined}
                     onValueChanged={(value) => {
                         setContent(value);
